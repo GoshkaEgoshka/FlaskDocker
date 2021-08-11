66e58b11d2853afac99358cb3f925b7c4ef5697c
Yehor <bingobingo1997@gmail.com>
2021-08-11 16:29:21 +0300

    Add logout functionality

--- web/login_helper.py
+++ web/login_helper.py
@@ -1,11 +1,17 @@
-from flask import session, redirect, url_for
+"""
+    Helper for login
+"""
 from functools import wraps
+from flask import session, redirect, url_for
+
 
 def login_check(func):
+    """
+        Check login decorator
+    """
     @wraps(func)
     async def decorated_function(*args, **kwargs):
         if not session.get('login', False):
             return redirect(url_for('get_authorization'))
         return await func(*args, **kwargs)
     return decorated_function
-   
